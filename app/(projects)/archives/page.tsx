347b94eaacb7361723046c735ae335e8c6c23835
= <dreyx0159@gmail.com>
2024-02-10 14:43:48 +0800

    updated

--- app/(projects)/archives/page.tsx
+++ app/(projects)/archives/page.tsx
@@ -1,4 +1,3 @@
-/* eslint-disable react-hooks/rules-of-hooks */
 'use client'
 
 import Link from 'next/link'
@@ -7,98 +6,166 @@ import { Plus } from 'lucide-react'
 import Image from 'next/image'
 import Archive from '@/components/archive'
 import { UserButton, useUser } from '@clerk/nextjs'
-import { Metadata } from 'next'
-
-
-export const metadata: Metadata = {
-   title: "Your archives",
-   description: "documents"
-}
-
-
-
+import { Button } from "@/components/ui/button"
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+ 
 export default function create() {
+    // eslint-disable-next-line react-hooks/rules-of-hooks
     const { isSignedIn, user } = useUser();
  
   return (
-    <div className='px-5 dark:text-white md:px-[55px] w-full py-4 md:py-[24px]'>
-       <nav className='flex justify-between items-center'>
-           <div className=''>
-              Logo
-           </div>
+    <div className="px-5 md:px-[55px] w-full py-4 md:py-[24px]">
+      <nav className="flex justify-between items-center">
+        <div className="">Logo</div>
 
-    {/**USER AUTHENTICATION */}
-            <ul className='flex gap-4 items-center'>
-               <li className='font-medium dark:text-white text-[18px] md:text-[22px] text-primaryColor'>
-                  <Link href={'/'}>Home</Link>
-               </li>
+        {/**USER AUTHENTICATION */}
+        <ul className="flex gap-4 items-center">
+          <li className="font-medium text-[18px] md:text-[22px] text-primaryColor">
+            <Link href={"/"}>Home</Link>
+          </li>
 
-           {isSignedIn || user ? 
-                  <div className='dark:border-white bottom-1 w-auto h-auto rounded-full'>
-                     <UserButton /> 
-                  </div> :
-                 <button className='bg-gradient-to-r from-violet-900 to-purple-500 hover:bg-violet-600 text-white px-3 rounded-sm py-2 '>
-                  <Link href={'/login'}>Login</Link>
-               </button>}
-            </ul>
-       </nav>
+          {isSignedIn || user ? (
+            <UserButton />
+          ) : (
+            <button className="bg-gradient-to-r from-violet-900 to-purple-500 hover:bg-violet-600 text-white px-3 rounded-sm py-2 ">
+              <Link href={"/login"}>Login</Link>
+            </button>
+          )}
+        </ul>
+      </nav>
 
+      {/**ARCHIVES */}
+      <div className="border-gray-20 shadow-lg border-[1px] rounded-md relative w-full min-h-screen mt-[50px] md:mt-[90px] mx-auto">
+        <div className="flex w-auto mt-[50px] mx-[10px] my-[15px] p-2 md:p-6 gap-8 items-start flex-col">
+          <h3 className="self-center sm:text-[25px] md:text-[27px]">
+            Your archives
+          </h3>
 
-     {/**ARCHIVES */}
-      <div className='border-gray-20 shadow-lg border-[1px] rounded-md relative w-full min-h-screen mt-[50px] md:mt-[90px] mx-auto' >
-           <div className='flex w-auto mt-[50px] mx-[10px] my-[15px] p-2 md:p-6 gap-8 items-start flex-col'>
-                <h3 className='self-center sm:text-[25px] md:text-[27px]'>Your archives</h3>
+          {/**ARCHIVES */}
+          <div className="w-full h-auto">
+            <div className="flex items-start gap-8 flex-col">
+              <Dialog>
+                <DialogTrigger asChild>
+                  {/**CREATE BUTTON HERE */}
+                  <div className="flex flex-col gap-2">
+                    <button
+                      disabled={!isSignedIn || !user}
+                      className={`${
+                        !isSignedIn || !user
+                          ? "bg-gray-500"
+                          : "bg-[#4B3F94] hover:bg-violet-700"
+                      } text-center w-[50px] h-[50px] sm:w-[80px] sm:h-[80px] duration-200 ease-in-out text-white rounded-md text-[18px]`}
+                      type="button"
+                    >
+                      <Plus size={32} className="m-auto" />
+                    </button>
+                    <span className="text-[#55545E] font-medium ">
+                      Create new
+                    </span>
+                  </div>
+                </DialogTrigger>
+                <DialogContent className="sm:max-w-[425px]">
+                  <DialogHeader>
+                    <DialogTitle>Create new document</DialogTitle>
+                    <DialogDescription>
+                      Make changes to your new work here. Click save when youre
+                      done.
+                    </DialogDescription>
+                  </DialogHeader>
+                  <div className="grid gap-4 py-4">
+                    <div className="grid grid-cols-4 items-center gap-4">
+                      <Label htmlFor="name" className="text-right">
+                        Name of your work
+                      </Label>
+                      <Input
+                        id="name"
+                        value="document name"
+                        className="col-span-3"
+                      />
+                    </div>
+                    <div className="grid grid-cols-4 items-center gap-4">
+                      <Label htmlFor="number" className="text-right">
+                        Number of document
+                      </Label>
+                      <Input
+                        id="documnumberent"
+                        value={0}
+                        type='number'
+                        className="col-span-3"
+                      />
+                    </div>
+                    <div className="grid grid-cols-4 items-center gap-4">
+                      <Label htmlFor="questions" className="text-right">
+                        Number of questions
+                      </Label>
+                      <Input
+                        id="questions"
+                        value={0}
+                        type='number'
+                        className="col-span-3"
+                      />
+                    </div>
+                  </div>
+                  <DialogFooter>
+                  {/**AFTER SUBMITTING THE FORM THEN GO TO THE MAIN PAGE WITH 
+                   * THE DETAILS ENTERED BY THE USER */}
+                    <Button type="submit" className='bg-primaryColor
+                    hover:bg-violet-400 duration-200 ease-in-out'>
+                      <Link href={'/main'}>Save changes</Link>
+                    </Button>
+                  </DialogFooter>
+                </DialogContent>
+              </Dialog>
 
-
-               {/**ARCHIVES */}
-                <div className='w-full h-auto'>
-                    <div className='flex items-start gap-8 flex-col'>
-                      {/**CREATE BUTTON HERE */}
-                     <div className='flex flex-col gap-2'>
-                        <button disabled={!isSignedIn || !user} className={`${!isSignedIn || !user ? 'bg-gray-500' : 'bg-[#4B3F94] hover:bg-violet-700'} text-center w-[50px] h-[50px] sm:w-[80px] sm:h-[80px] duration-200 ease-in-out text-white rounded-md text-[18px]`} type='button'>
-                          <Plus 
-                            size={32}
-                            className='m-auto'
-                          />
-                        </button>
-                        <span className='text-[#55545E] font-medium '>Create new</span>
-                     </div>
-
-
-                        {/**ARCHIVES LISTS */}
-                       {isSignedIn || user ?
-                        <div className='border scroll-auto w-auto min-w-full max-w-[1180px] min-h-[700px]'>
-                              {/**archive items */}
-                              <div className='h-auto w-fit min-w-max'>
-                                 {/**START MAPPING ITEMS HERE */}
-                                 <Archive />
-                              </div>
-                        </div> : <p className='self-center mx-auto mt-[70px] text-[18px] font-normal text-[#A09EA8]'>Log in to continue</p>}
-                    </div> 
+              {/**ARCHIVES LISTS */}
+              {isSignedIn || user ? (
+                <div className="border scroll-auto w-auto min-w-full max-w-[1180px] min-h-[700px]">
+                  {/**archive items */}
+                  <div className="h-auto w-fit min-w-max">
+                    {/**START MAPPING ITEMS HERE */}
+                    <Archive />
+                  </div>
                 </div>
-           </div>
+              ) : (
+                <p className="self-center mx-auto mt-[70px] text-[18px] font-normal text-[#A09EA8]">
+                  Log in to continue
+                </p>
+              )}
+            </div>
 
-           {/**PROPS IMAGES */}
-           <Image 
-              src={'/book.svg'}
-              alt='book avatar'
-              width={100}
-              height={50}
-              quality={100}
-              className='absolute top-1 right-2 w-[50px] md:w-auto'
-           />
+          </div>
+        </div>
 
-         <Image 
-              src={'/woman-with-book.svg'}
-              alt='book avatar'
-              width={100}
-              height={100}
-              quality={100}
-              sizes='100vw'
-              className='absolute left-2 bottom-0 w-[70px] md:w-auto'
-           />
+        {/**PROPS IMAGES */}
+        <Image
+          src={"/book.svg"}
+          alt="book avatar"
+          width={100}
+          height={50}
+          quality={100}
+          className="absolute top-1 right-2 w-[50px] md:w-auto"
+        />
 
+        <Image
+          src={"/woman-with-book.svg"}
+          alt="book avatar"
+          width={100}
+          height={100}
+          quality={100}
+          sizes="100vw"
+          className="absolute left-2 bottom-0 w-[70px] md:w-auto"
+        />
       </div>
     </div>
-  )
+  );
 }
--- app/components/navbar-main.tsx
+++ app/components/navbar-main.tsx
@@ -1,12 +1,10 @@
-'use client'
-
-import { UserButton, currentUser, useUser } from '@clerk/nextjs'
+import { UserButton,  useUser } from '@clerk/nextjs'
 import Link from 'next/link'
 import React from 'react'
 
 export default function NavbarMain() {
 
-const { user } = useUser();
+    const { user, isLoaded } = useUser();
   return (
     <div className='w-full bg-[#6B5DC0] h-[60px] flex items-center'>
         <nav className='px-[12px] md:px-12 text-white py-2 flex justify-between items-center w-full'>
@@ -15,10 +13,13 @@ const { user } = useUser();
            <div>
               ReThink
            </div>
-
-           {user? <UserButton /> : 
+  
+           <div className='flex items-center justify-between gap-6'>
+           <Link href={'/'} className='text-md md:text-[20px]'>Home</Link>
+           {user && isLoaded? <UserButton /> : 
              <Link href={'/login'} className='text-md md:text-[20px]' >Log in</Link>
-           }
+           }  
+          </div>  
         </nav>
     </div>
   )
